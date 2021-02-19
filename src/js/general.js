037d384139bbef63b072e5ad979dd4da71066100
umut <askarbekova.umut.kal@gmail.com>
2021-02-19 21:32:50 +0600

    Страница Dapp

--- src/js/general.js
+++ src/js/general.js
@@ -1,5 +1,7 @@
 $(function () {
     console.log("xxxxxxxxxx");
     $('select').selectpicker();
-
+    $('.selectpicker').on('changed.bs.select', function (e) {
+        $(this).prevAll('.dropdown-toggle').toggleClass('bs-placeholder', this.value === '');
+    }).trigger('changed.bs.select');
 });
